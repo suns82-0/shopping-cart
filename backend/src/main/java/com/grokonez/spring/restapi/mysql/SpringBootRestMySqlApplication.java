4dc69c6447a2c26aa1ac42229760add65a2b09f9
孙兴 <sunxing@s-MacBook-Pro-2.hsd1.ca.comcast.net>
2019-10-10 13:26:24 -0700

    Initial commit

---
+++ backend/src/main/java/com/grokonez/spring/restapi/mysql/SpringBootRestMySqlApplication.java
@@ -0,0 +1,12 @@
+package com.grokonez.spring.restapi.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestMySqlApplication.class, args);
+	}
+}
